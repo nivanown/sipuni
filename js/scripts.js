e102ba6adbafee96a27527e411d05fa71ea6df85
Nikita <32540777+nivanown@users.noreply.github.com>
2023-04-13 19:26:11 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+﻿/*- reviews-slider -*/
+var swiper = new Swiper(".reviews-slider", {
+  autoHeight: true,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".reviews-slider .swiper-button-next",
+    prevEl: ".reviews-slider .swiper-button-prev",
+  },
+});
+
+/*- mov-slider -*/
+var swiper = new Swiper(".mov-slider", {
+  autoHeight: true,
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 60,
+});
